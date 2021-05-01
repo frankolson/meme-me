906fca994fbdbf04c3f47de7a5d1b734cef98112
Will Olson <will.olson11@gmail.com>
2021-05-01 08:34:33 -0700

    Use dot notation while setting default text attributes

--- Meme Me v1/ViewController.swift
+++ Meme Me v1/ViewController.swift
@@ -109,10 +109,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func setTextFieldAttributes(_ textField: UITextField, startText: String) {
         let memeTextAttributes: [NSAttributedString.Key: Any] = [
-            NSAttributedString.Key.strokeColor: UIColor.black,
-            NSAttributedString.Key.foregroundColor: UIColor.white,
-            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-            NSAttributedString.Key.strokeWidth:  -5.0
+            .strokeColor: UIColor.black,
+            .foregroundColor: UIColor.white,
+            .font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            .strokeWidth:  -5.0
         ]
         
         textField.delegate = self
