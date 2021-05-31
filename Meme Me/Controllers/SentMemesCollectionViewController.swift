0303f6221de10e671ec4ac7edc01475ec9c51fc4
Will Olson <will.olson11@gmail.com>
2021-05-31 14:38:19 -0500

    Add new controller files

---
+++ Meme Me/Controllers/SentMemesCollectionViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SentMemesCollectionViewController.swift
+//  Meme Me
+//
+//  Created by Will Olson on 5/31/21.
+//
+
+import Foundation
---
+++ Meme Me/Controllers/SentMemesTableViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SentMemesTableViewController.swift
+//  Meme Me
+//
+//  Created by Will Olson on 5/31/21.
+//
+
+import Foundation
