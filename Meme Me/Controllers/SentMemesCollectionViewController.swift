be51d0adde3bfe6073238cb9062cc6ce7b18e3e7
Will Olson <will.olson11@gmail.com>
2021-06-03 20:01:42 -0700

    Hooked up and designed collection view

--- Meme Me/Controllers/SentMemesCollectionViewController.swift
+++ Meme Me/Controllers/SentMemesCollectionViewController.swift
@@ -9,5 +9,53 @@ import Foundation
 import UIKit
 
 class SentMemesCollectionViewController: UICollectionViewController {
-    // TODO
+    // MARK: Properties
+    
+    var allMemes: [Meme] {
+        return (UIApplication.shared.delegate as! AppDelegate).memes
+    }
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    // MARK: Life cycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+        
+        let space:CGFloat = 3.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
+        
+        // This only works if the segue is presented with a "Fullscreen" presentation
+        // source: https://stackoverflow.com/a/51089181/3347828
+        self.collectionView.reloadData()
+    }
+    
+    // MARK: Collection Data Source methods
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return allMemes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "memeCollectionCell", for: indexPath) as! CollectionViewCell
+        let meme = self.allMemes[(indexPath as NSIndexPath).row]
+        
+        // Set the name and image
+        cell.memeImageView?.image = meme.memeImage
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailController = self.storyboard!.instantiateViewController(withIdentifier: "MemeDetailViewController") as! MemeDetailViewController
+        detailController.meme = self.allMemes[(indexPath as NSIndexPath).row]
+        self.navigationController!.pushViewController(detailController, animated: true)
+    }
 }
---
+++ Meme Me/Views/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Meme Me
+//
+//  Created by Will Olson on 6/3/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Outlets
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+}
